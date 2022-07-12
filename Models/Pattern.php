469041138bee85103cbaaff69ce585034343af43
mirsengar <Mirsengar>
2022-07-13 01:49:31 +0430

    first commit

---
+++ Models/Pattern.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Sms\IpPanel\Models;
+
+class Pattern extends Base
+{
+    /**
+     * Pattern unique code
+     * @var string
+     */
+    public $code;
+
+    /**
+     * Pattern status
+     * @var string
+     */
+    public $status;
+
+    /**
+     * Pattern content
+     * @var string
+     */
+    public $message;
+
+    /**
+     * Pattern shared or not
+     * @var bool
+     */
+    public $isShared;
+}
---
+++ Models/Recipient.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Sms\IpPanel\Models;
+
+class Recipient extends Base
+{
+    /**
+     * Recipient number
+     * @var string
+     */
+    public $recipient;
+
+    /**
+     * Recipient delivery status
+     * @var string
+     */
+    public $status;
+}
